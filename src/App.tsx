3cd540a4a061b6b90b77ebe387084c2345490f5d
William Tonglet <william.tonglet@wipro.com>
2020-05-24 23:37:49 -0300

    volta ao commit correto

--- src/App.tsx
+++ src/App.tsx
@@ -7,22 +7,30 @@ function App() {
     <div className="App">
       <RevealTransition position="left">
         <section className="section1">
-          <h1>Scroll Down</h1>
+          <RevealTransition position="top" delay={800}>
+            <h1>Scroll Down</h1>
+          </RevealTransition>
         </section>
       </RevealTransition>
       <RevealTransition position="right">
         <section className="section2">
-          <h1>Scroll Down</h1>
+          <RevealTransition position="bottom" delay={800}>
+            <h1>Scroll Down</h1>
+          </RevealTransition>
         </section>
       </RevealTransition>
       <RevealTransition position="left">
         <section className="section1">
-          <h1>Scroll Down</h1>
+          <RevealTransition position="top" delay={800}>
+            <h1>Scroll Down</h1>
+          </RevealTransition>
         </section>
       </RevealTransition>
       <RevealTransition position="right">
         <section className="section2">
-          <h1>Scroll Down</h1>
+          <RevealTransition position="bottom" delay={800}>
+            <h1>Scroll Down</h1>
+          </RevealTransition>
         </section>
       </RevealTransition>
     </div>
